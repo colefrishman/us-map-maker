be6c66f5d66bc3da765414224a7a51a258bab4d8
Cole Frishman <awesomeeloc@gmail.com>
2021-01-05 22:19:38 -0500

    Option to show whether no data option; refactoring; limit inputs to on;ly necessary ones

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
 	const [color, setColor] = useState("Reds");
 	const [categories, setCategories] = useState(3);
 	const [colorScheme, setColorScheme] = useState(["#aaaaaa", "#fee0d2", "#fc9272", "#de2d26"]); 
+	const [excludeNoData, setExcludeNoData] = useState(0);
 	const [legend, setLegend] = useState(["No data", "Cat. 1", "Cat. 2", "Cat. 3", "Cat. 4", "Cat. 5", "Cat. 6", "Cat. 7", "Cat. 8", "Cat. 9"]); 
 
 	const updateScheme = (colr, cats) => {
@@ -23,6 +24,7 @@ function App() {
 		else if(tempScheme[0] !== "#ffffff" && colr === "Greys"){
 			tempScheme.unshift("#ffffff");
 		}
+
 		
 		setColorScheme(tempScheme);
 	}
@@ -40,12 +42,21 @@ function App() {
 		document.body.appendChild(e);
 	}
 
+
+	const LegendInput = () => {
+		let temp = []
+		for (let i=excludeNoData; i<categories; ++i){
+			temp.push(<Form.Control type="text" value={legend[i]} maxLength="16" onChange={(event)=>{updateLegend(i, event.target.value)}} />)
+		}
+		return temp;
+	}
+	
 	return(
 		<div>
 			<h1>stats</h1>
 			<Button onClick={() => {downloadSvg()}}>Download as SVG</Button>
 			<br />
-			<Svg colorScheme={colorScheme} title={title} legend={legend}/>
+			<Svg colorScheme={colorScheme} title={title} legend={legend} excludeNoData={excludeNoData}/>
 			<br />
 			<Form.Label>sequential: </Form.Label>
 			<br />
@@ -65,8 +76,11 @@ function App() {
 			<Button style={{color:"white", backgroundColor:"#e41a1c", borderColor:"#377eb8"}} onClick={() => {updateScheme("Set1",categories)}}>dark A</Button>
 			<Button style={{color:"white", backgroundColor:"#fbb4ae", borderColor:"#b3cde3"}} onClick={() => {updateScheme("Pastel1",categories)}}>pastel A</Button>
 			<Button style={{color:"white", backgroundColor:"#8dd3c7", borderColor:"#ffffb3"}} onClick={() => {updateScheme("Set3",categories)}}>light</Button>
+			
+			<Form.Check type="checkbox" label="Exclude 'No data' checkbox" onChange={() => {setExcludeNoData(1-excludeNoData)}} checked={excludeNoData} />
+			<Form.Label>categories</Form.Label>
 
-			<Form.Control as="select" onChange={(event)=>{updateScheme(color, event.target.value)}}>
+			<Form.Control as="select" onChange={(event)=>{updateScheme(color, event.target.value)}} value={categories}>
     			<option>3</option>
     			<option>4</option>
     			<option>5</option>
@@ -77,20 +91,11 @@ function App() {
     		</Form.Control>
 			
 			<Form.Label>title</Form.Label>
-			<Form.Control type="text" onChange={(event)=>{setTitle(event.target.value)}} />
+			<Form.Control type="text" onChange={(event)=>{setTitle(event.target.value)}} value={title}/>
 
 			<Form.Label>Legend Labels</Form.Label>
-			<Form.Control type="text" maxLength="16" onChange={(event)=>{updateLegend(0, event.target.value)}} />
-			<Form.Control type="text" maxLength="16" onChange={(event)=>{updateLegend(1, event.target.value)}} />
-			<Form.Control type="text" maxLength="16" onChange={(event)=>{updateLegend(2, event.target.value)}} />
-			<Form.Control type="text" maxLength="16" onChange={(event)=>{updateLegend(3, event.target.value)}} />
-			<Form.Control type="text" maxLength="16" onChange={(event)=>{updateLegend(4, event.target.value)}} />
-			<Form.Control type="text" maxLength="16" onChange={(event)=>{updateLegend(5, event.target.value)}} />
-			<Form.Control type="text" maxLength="16" onChange={(event)=>{updateLegend(6, event.target.value)}} />
-			<Form.Control type="text" maxLength="16" onChange={(event)=>{updateLegend(7, event.target.value)}} />
-			<Form.Control type="text" maxLength="16" onChange={(event)=>{updateLegend(8, event.target.value)}} />
-			<Form.Control type="text" maxLength="16" onChange={(event)=>{updateLegend(9, event.target.value)}} />
-
+			
+			{LegendInput()}
 
 			<p>Map outline from simplemaps.com</p>
 			<p>Colors from colorbrewer2.org</p>
